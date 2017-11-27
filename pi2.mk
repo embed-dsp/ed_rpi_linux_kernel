c06d6e48a085f81a8cc25466cb4d6e7246039413
Gudmundur Bogason <gb@embed-dsp.com>
2017-11-27 08:49:16 +0100

    Renamed pi7.mk to pi3.mk and created pi2.mk.

---
+++ pi2.mk
@@ -0,0 +1,17 @@
+
+# Copyright (c) 2017 embed-dsp
+# All Rights Reserved
+
+# $Author:   Gudmundur Bogason <gb@embed-dsp.com> $
+# $Date:     $
+# $Revision: $
+
+# This makefile is used to cross compile kernels for the following raspberry pi models:
+#   Raspberry Pi 2 Model B
+
+KCFLAGS = "-march=armv7-a -mtune=cortex-a7"
+
+KERNEL = kernel7
+DEFCONFIG = bcm2709_defconfig
+
+include pi-common.mk
